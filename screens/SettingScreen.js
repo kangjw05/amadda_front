8f369f0170a8d5b7d0225125cd11f81709582613
강지원 <kangjw05@naver.com>
2025-05-03 23:40:24 +0900

    설정 페이지 파일일, 페이지 스타일 추가가

---
+++ screens/SettingScreen.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList } from "react-native";
+import { themeColors, categories, groups } from "../Colors";
+
+import SettingScreenStyles from './SettingScreenStyles';
+
+const SettingScreen = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default SettingScreen;
---
+++ styles/SettingScreenStyles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { themeColors, categories, groups } from "../Colors";
+
+const SettingScreenStyles = StyleSheet.create({
+
+});
+
+export default SettingScreenStyles;
