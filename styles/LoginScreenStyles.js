bef78fcf876ae983868da6a983b34def4d06f563
강지원 <kangjw05@naver.com>
2025-05-03 23:39:46 +0900

    로그인 페이지, 페이지 스타일 추가가

---
+++ styles/LoginScreenStyles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { themeColors, categories, groups } from "../Colors";
+
+const LoginScreenStyles = StyleSheet.create({
+
+});
+
+export default LoginScreenStyles;
