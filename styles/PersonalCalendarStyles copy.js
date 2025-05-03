69c2c7da13f2ce167294b4d911d4326fd468448a
강지원 <kangjw05@naver.com>
2025-05-03 23:40:02 +0900

    개인 캘린더 페이지, 페이지 스타일 추가가

---
+++ styles/PersonalCalendarStyles copy.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { themeColors, categories, groups } from "../Colors";
+
+const PersonalCalendarStyles = StyleSheet.create({
+
+});
+
+export default PersonalCalendarStyles;
