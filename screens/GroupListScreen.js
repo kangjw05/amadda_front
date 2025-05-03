038bdba6b81ef447fc319f39555afda8929330bf
강지원 <kangjw05@naver.com>
2025-05-03 23:39:05 +0900

    그룹 리스트 화면, 화면 스타일 추가

---
+++ screens/GroupListScreen.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList } from "react-native";
+import { themeColors, categories, groups } from "../Colors";
+
+import GroupListScreenStyles from './GroupListScreenStyles';
+
+const GroupListScreen = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default GroupListScreen;
