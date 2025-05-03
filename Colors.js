ab7df21facc40f9ee9da9a8cf93cdad1287ba68f
강지원 <kangjw05@naver.com>
2025-05-03 23:22:31 +0900

    Colors.js 추가

---
+++ Colors.js
@@ -0,0 +1,35 @@
+export const themeColors = {
+    bg: '#F9F9F9',
+    bar: '#ECF6FD',
+    highlight: '#555B8F',
+    text: '#3E4062',
+    lightText: '#B3B8E2',
+    sunday: '#AA5353',
+    saturday: '#212CE5',
+    tag: '#E6E6E6',
+}
+
+export const categories = {
+    category1: { bg: '#C3DFF0', text: '#6488BB', checkbox: '#7EB4BC' },
+    category2: { bg: '#DDA59F', text: '#89372E', checkbox: '#914B4C' },
+    category3: { bg: '#97B08A', text: '#465F3A', checkbox: '#4D7745' },
+    category4: { bg: '#65B9B8', text: '#2F5454', checkbox: '#2F7372' },
+    category5: { bg: '#CEC4D7', text: '#42325F', checkbox: '#5E2F73' },
+    category6: { bg: '#F5F4C3', text: '#5D5F32', checkbox: '#8C972D' },
+    category7: { bg: '#D5BFCF', text: '#5F3255', checkbox: '#972D70' },
+    category8: { bg: '#E4E4E4', text: '#464646', checkbox: '#353535' },
+};
+
+
+export const groups = {
+    group1:  { bg: '#638A7E', text: '#DBF0E4', checkbox: '#324B25' },
+    group2:  { bg: '#DBF0E4', text: '#324B25', checkbox: '#638A7E' },
+    group3:  { bg: '#788559', text: '#F3FCD5', checkbox: '#414C33' },
+    group4:  { bg: '#F3FCD5', text: '#414C33', checkbox: '#788559' },
+    group5:  { bg: '#DFCC98', text: '#FFFBEC', checkbox: '#857659' },
+    group6:  { bg: '#FFFBEC', text: '#857659', checkbox: '#DFCC98' },
+    group7:  { bg: '#CC7161', text: '#FFE4D6', checkbox: '#913D3D' },
+    group8:  { bg: '#FFE4D6', text: '#913D3D', checkbox: '#CC7161' },
+    group9:  { bg: '#717FAF', text: '#D9E1FF', checkbox: '#383982' },
+    group10: { bg: '#D9E1FF', text: '#383982', checkbox: '#717FAF' },
+}
