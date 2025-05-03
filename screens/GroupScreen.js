b3cca4945bf6c372e6e332d76564d3f8dda0345d
강지원 <kangjw05@naver.com>
2025-05-03 23:39:32 +0900

    그룹 상세 페이지, 페이지 스타일 추가가

---
+++ screens/GroupScreen.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList } from "react-native";
+import { themeColors, categories, groups } from "../Colors";
+
+import GroupScreenStyles from './GroupScreenStyles';
+
+const GroupScreen = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default GroupScreen;
---
+++ styles/GroupScreenStyles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { themeColors, categories, groups } from "../Colors";
+
+const GroupScreenStyles = StyleSheet.create({
+
+});
+
+export default GroupScreenStyles;
