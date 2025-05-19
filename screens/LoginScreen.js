a3a1bf811dd439da034bf4e8067e0ec075160d38
0PP415 <majw0415@gmail.com>
2025-05-19 16:09:52 +0900

    Initial files re-commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  FlatList,
+} from "react-native";
 import { themeColors, categories, groups } from "../Colors";
 
-import styles from './LoginScreenStyles';
+import styles from "../styles/LoginScreenStyles";
 
 const LoginScreen = () => {
-    return (
-    <View>
-      
-    </View>
-    );
+  return <View></View>;
 };
 
 export default LoginScreen;
