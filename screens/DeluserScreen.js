24306ce5162a05b34b0aa47a92fb612fd1158e20
강지원 <kangjw05@naver.com>
2025-07-11 00:38:06 +0900

    이메일 입력 개수 제한 수정

--- screens/DeluserScreen.js
+++ screens/DeluserScreen.js
@@ -134,7 +134,7 @@ const DeluserScreen = () => {
               style={styles.textInput}
               value={email}
               onChangeText={setEmail}
-              maxLength={20}
+              maxLength={45}
             />
           </ImageBackground>
           <TouchableOpacity
